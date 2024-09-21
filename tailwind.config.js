e4295394baf33d9c99815b458cd09b93a0e23031
khaymanii <kennethakpo61@gmail.com>
2024-09-21 04:35:32 +0100

    change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
   content: ["./App.{js,jsx,ts,tsx}", "./screens/**/*.{js,jsx,ts,tsx}"],
-=======
-  content: [
-    "./App.{js,jsx,ts,tsx}",
-    "./screens/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}",
-  ],
->>>>>>> 83044fb40b8725dd49e09315ebb76a474f98c9eb
   theme: {
     extend: {},
   },
