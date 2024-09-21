26290132b578ecde0192dea3ad935b2d394b07d3
khaymanii <kennethakpo61@gmail.com>
2024-09-21 04:47:08 +0100

    fixe

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-blue-400">
+    <View className="flex-1 items-center justify-center bg-green-400">
       <Text className="">React native</Text>
     </View>
   );
