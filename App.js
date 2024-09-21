145c5dc28763bec698e8091469ba2afeead935cf
khaymanii <kennethakpo61@gmail.com>
2024-09-21 04:41:27 +0100

    created another branch

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-blue-400">
-      <Text className="text-4xl">react native</Text>
+      <Text className="">React native</Text>
     </View>
   );
 }
