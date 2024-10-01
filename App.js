dcbf530e92a3f2e0f4aa9df61173cac818017bf6
khaymanii <kennethakpo61@gmail.com>
2024-10-01 23:36:04 +0100

    created auth screen

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import tw from "twrnc";
-import { View } from "react-native";
-import Welcome from "./screens/Welcome";
+import { View, Text } from "react-native";
+import Authentication from "./screens/Authentication";
 export default function App() {
   return (
     <View style={tw`flex-1`}>
-      <Welcome />{" "}
+      <Authentication />{" "}
     </View>
   );
 }
