46068e7fd158761c12858675af3cf861058ec63f
khaymanii <kennethakpo61@gmail.com>
2024-10-01 17:28:59 +0100

    fix

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
 import React from "react";
 import tw from "twrnc";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 export default function App() {
   return (
     <View style={tw`flex-1 justify-center items-center`}>
-      <Image
-        source={require("./assets/icons/logo.svg")}
-        style={tw`w-50 h-50`}
-        resizeMode="cover"
-      />
+      <Text>Fruithub</Text>
     </View>
   );
 }
