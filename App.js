3f04312d8a7e10c4aebc3476af57c1c10c053c1b
khaymanii <kennethakpo61@gmail.com>
2024-10-01 17:26:46 +0100

    finnished

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
-import { Text, View } from "react-native";
-
+import React from "react";
+import tw from "twrnc";
+import { View } from "react-native";
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-green-400">
-      <Text className="">React native</Text>
+    <View style={tw`flex-1 justify-center items-center`}>
+      <Image
+        source={require("./assets/icons/logo.svg")}
+        style={tw`w-50 h-50`}
+        resizeMode="cover"
+      />
     </View>
   );
 }
--- screens/Splash.jsx
+++ screens/Splash.jsx
@@ -1,10 +1,15 @@
-import { View, Image } from "react-native";
+import { View, Image, Text } from "react-native";
 import React from "react";
+import tw from "twrnc";
 
 export default function Splash() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Image source={require("../assets/icons/logo.svg")} className="" />
+    <View style={tw`flex-1 justify-center items-center`}>
+      <Image
+        source={require("../assets/icons/logo.svg")}
+        style={tw`w-50 h-50`}
+        resizeMode="cover"
+      />
     </View>
   );
 }
