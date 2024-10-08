22977b1dbc1ede7f5a6f1c799959b2c9c092d5e1
khaymanii <kennethakpo61@gmail.com>
2024-10-08 09:20:36 +0100

    added png icons

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,13 +1,15 @@
 import { Text, View, Image } from "react-native";
 import React from "react";
 import tw from "twrnc";
-import BarLogo from "../assets/icons/bar.svg";
 
 export default function Home() {
   return (
     <View>
       <View>
-        <Image source={BarLogo} style={tw`w-[20px] h-[20px] mx-4`} />
+        <Image
+          source="../assets/icons/bar.png"
+          style={tw`w-[20px] h-[20px] mx-4`}
+        />
         <View></View>{" "}
       </View>
     </View>
